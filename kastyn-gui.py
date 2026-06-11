8e09a84e65e5d96bc89270cd7789160882a75dfb
thecre8ivemonk-cre8 <thecre8ivemonk@gmail.com>
2026-06-11 21:50:26 +0100

    Add Kastyn logo to GUI and exe icon

--- kastyn-gui.py
+++ kastyn-gui.py
@@ -7,6 +7,9 @@ import json
 import sys
 import platform
 import urllib.request
+from PIL import Image, ImageTk
+
+LOGO_B64 = "iVBORw0KGgoAAAANSUhEUgAAAQAAAAEACAYAAABccqhmAAAABmJLR0QA/wD/AP+gvaeTAAAQcUlEQVR4nO3de5hcdX3H8c/3zGSzOzO7uSiKXJTog2AE5CIqpShYipSL+liaRypP6G5CA9LHPjyo5doGEZSL2kpTGgm7lNIWQ2mh4SkURFAKFrQBS4ykVCwBFMQkZHdm9jZzfv2DLN1sdmdnZ+ec3+w579dfu7Nzfue7yZzPnnN+l2Oag9x6ZYb7u5ZUM5V3m+xg5+zdkjtAsgWSy0uWl9wiSXlJbZ7Lxdy3U1LonF41c7+S9LLJfhZKWwKFmzts8MfWrSHfRTbCfBdQj223Le5qHx36iHN2gknHS3qvOLDROkYlPeWkx8zc/blM+Xu2XCXfRdWjZQOgeFP+MMvo05I+Kun9kjKeSwLqNSzpQSetH7V5dy/qfu013wVNpaUCYOe6rsXZoHKGnJbL7Fjf9QBNMCLpbnPhN3IrBn/gu5iJWiIAir35E026QNJJkrK+6wGi4f7dXHBDxwvFf7LVqviuRvIYAM7Jyr2502V2iaQP+qoD8OBFyV2RK5T7bJmqPguJPQCckw32FZY5uUslHRr3/oEWssmZvlDoLt3nq4BYA2BgXeEQC9xfmXRcnPsFWtwDzukLhRWlH8e941gCwK1VrpwtfFHmLhbdd8BkKiZ9raNQ+lNbppG4dhp5AJR6c6dJdqOk/aLeF5AAG8PAlnf+QfEncewssgBwq5Ut71+4TOYulxREtR8ggYacaXU+X7o+6puEkQTA4E3tS8Js5nY5fSCK9oGU+LcRm/fpKAcSNT0ASn25T8pZn6SFzW4bSBuTtgSWOb29u//ZKNpv6ql56ebC+XJ2pzj4gaZw0kFVV3282Js/MYr2mxIAzslKvYUvy9xfNqtNAG9YZNK9pd7Cec1ueNaXAG69MuViYY3kVjWjIAC1uMvyPeWrmtXarALg9YM//w+Sfq9J9QCYhnO6uLCi9NVmtNXw6bpzsvJA4UZx8AOxMtNXSn25P2tGWw0HQLmvcKXMndOMIgDMkLPVpd7cZbNtpqFLgFJf4bNybs1sdw5gttyqfE/5W41uPeMA2NXPf6e42w+0gpHQ7GOd3cWHG9l4RgEweGvH28NK8KSkxY3sDEAkdoah/WbnyuKmmW5Y919xt15tYTW4Qxz8QKtZEAThXf1rO9880w3rDoBSKX8NY/uBVmXvymTDv3VuZmf1dQVAqS93ujn9cWOFAYiF6eTBW/Kfm9km03BrlSvPy2+W9I6GCwMQl2Fn+lChu/RUPW+e9gxgcF7+S+LgB+aK+XJa79bsVajnzTUDYGBd4RAnzeiUAoBfJh1Ybi9dXc97pwwA52SZwN0gaV7TKgMQD7PPltbljpjubVMGwOtLd+v4phYFIC4ZBcEat7r2Wf6kP3ROtmvdfgBzljumvH+uu9Y7Ju0FKN2c+7jM7o6mKAAx+nUlzBy0YGX/9sl+OPnpgelPIi0JQFzenA2qn5/qh3ucAex6UOcD0dYEID6uGCp4Z2dP8dWJP9njDGDXU3oBJIYVAk0+kne3M4DiTfm3WkYvikd0A0nTXwkzSybeC9jtDCDI6jPi4AeSqCtr4R6D+nYLAOe0PL56AMTK3Cq3dveBfW8EwMC6wiGS3hd7UQDisne5LXfy+BfeCIDAdGb89QCIlQtWjv/2/y8BAhfJo4cAtBJ3Sunm3D5j3wWStH3togVyOtJfUQBiknWyz4x9E0jS/LbhD4u7/0A6mE4d+zKQJOfsBH/VAIiTScduX7togbQrAIxpv0CaZNvbRn9LkgK3XhlJSz0XBCBGLtTJkhQM93ctkTTfcz0A4rSr1y+oqnKQ71oAxMzpgB19CxcGljECAEgfm++GlwbOEQBAGjnZIYHkWPMfSKdDA8m6fFcBwAc7MJBcp+8yAHixMJBEAAApZAQAkF5uVwDU9RBBAImzMJDU5rsKAF7Mn/bx4ACSiwAAUowAAFKMAABSjAAAUowAAFKMAABSLNUrAb+wzemeJyu677+qev7XTr/YESoTmN7+JtN79g10xgeyOunQjNpS/a+EJLNSb975LiJuL+1w+vJdI/q7RyuqhrXf+5Yu01XL2nTmMVmZ1X4vMNekLgA2bKxq5U3DKg7P7Nc+fmlGt503X4vypACSI1X3ANY8MKrfXzM044Nfkh7eXNWJXxnSi9tTlZdIuNQEwIaNVV10+4jCWRy/z/wi1GnXD6l/kBBAMqQiAF7a4bTypuFZHfxjnn051Hm9I7NvCGgBqQiAK/95pKHT/qnc9Z8V3fNktWntAb4kPgBe2Ob0949Vmt7u5f84oso0PQhAq0t8D/eGJ6fv6mvEf/8y1Pd/WtVH35tpfuNzBOMo5r7E/9fc/3R0p+p3/rCSygCoPY7CadOLTpteDHXH4xXGUbS4xF8CPPdKdHfsf/Rc+q4BNmys6shLBnXrI/WdWf2q3+mcdcM67WtD2lGi96TVJD4AXtkZ3Ydu67Z0faAZR5E8iQ+AZt79nyhN4wEYR5FMiQ8AzB7jKJKLAMC0GEeRXAQAamIcRbIRAKgp6nEU8IsAQE1Rj6OAXwQAamIcRbIRAKiJcRTJRgCgJsZRJBsBAKQYAQCkGAEApFjipwPPZcy3R9T46LQg5tsjLlwCtBjm2yNOBEALYb494kYAtAjm28MHAqAFMN8evhAALYD59vCFAPCM+fbwiQDwjPn28IkA8Iz59vCJAPCM+fbwiQDwjPn28IkA8Iz59vCJAABSjAAAUowAAFKM6cBILNZTmF6Kf3UkFesp1I9LACQK6ynMDAGAxGA9hZkjAJAIrKfQGAIAcx7rKTSOAMCcx3oKjSMAMKexnsLsEACY01hPYXYIAMxprKcwOwQA5jTWU5gdAgBzGuspzA4BgDmN9RRmhwAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAABSjAAAUowAAFKMAMCc1tVhkbXdGWHbrYIAwJy23+LoDtK3LSQAgJZ29Duj+wgv2YsAAFrap47ORtb277wvE1nbrYIAwJx2/NKMDty7+R/jbCCdcnh04dIqCADMadlAuvKMtqa3e/aHs9p3EZcAQMs7/ciMPnFU8/5ad3aYLvlE80OlFREASIRvrWzTe/ad/cc5MKn3nPnae0Hy//pLBAASojDfdNcF7Tp4n8Y/0oFJ157ZplMOT/7NvzEEABJjv8Wm71zcrhOWzvwA7uwwrf9cu847cV4ElbUuAgCJsihv2nBhu9atnK+31nEanw2kno9k9dTVHano9pso+f0cSB0z6czfyOqMD2Z1/9NV3fF4RT/8WVUv73TKBqZ9FpuW7GU6+bCMTj0iHXf7p0IAILHmZaRTD8/o1BRd088UlwBAihEAQIoRAECKEQCeMZ8dPhEAnjGfHT4RAJ4xnx0+EQCeMZ8dPhEAnjGfHT4RAJ4xnx0+EQAtgPns8IUAaBHMZ4cPBECLYD47fCAAWgjz2RE3AqDFMJ8dcaKfqAUxnx1xIQBaGPPZETUuAYAUIwCAFCMAgBQjAFAT6xUkGwGAmlivINkIANTEegXJRgCgJtYrSDYCADWxXkGyEQCoifUKko0AwLRYryC5CADUhfUKkinxAUA/dnOwXkEyJT4A6MduHtYrSJ7EBwD92M3FegXJkvh+mE8dndXfPFKJpO20fqBZryA5rNSbd76LiFIllN5/2aCefTlsarvZQNp8XY4PN+a0xF8C0I8NTC3xASDRjw1MJRUBINGPDUwmNQFAPzawp9QEgEQ/NjBR4nsBJuOcdPsPKrr0jhG9srP2r58NpOXHZXXpJ9s47UfipDIAxoxWRT82Ui3VAQCkXaruAQDYHQEApBgBAKQYAQCkGAEApBgBAKQYAQCkWCBp2HcRALwYDiS95rsKAF4MBM5pp+8qAHhgGgjMCAAglUINBDLt8F0HAB/cQKDQ/Y/vMgB4YNYfyIJNvusA4IP73yBw9rTvMgDEz8y2BIOV7NOSWBMASJlQ2mKSVOrN/1zSAX7LARCnoFrd9eA804OeawEQr6H2BUNbA0kyZ/f5rgZArH5iy1QNJGmwre1+SaOeCwIQEzN9V9o1G/BNZ23vl9zjfksCEBcXuoekcdOBnWyDv3IAxGg0N5R/RBq/HoDpVkkVXxUBiIlzT9j5rxalcQFQ6C69LOleb0UBiEcQfPeNL3f7gbne2IsBEKuwqvVjX+8WALnny/fI9Mv4SwIQkx91riy+Mf9ntwCw1apIti7+mgDEwUy3jf9+j0VBK9Xgz00aiK8kADGphBXdPv6FPQJgwcr+7TKtia8mADG5t3BO6ZXxL0y6LHglCK7nLABIltDs6xNfmzQAus4e2CbTX0dfEoB42H90dhcfnvjqlA8GqQTBNZK2RVkSgJi48EuTvTxlAHSdPbBNcpdEVxGAmDyV6ylPOuO35qPBclvL6yQxSQiYy8xdYTb5ql823bal3tyRkj0hKdP0wgBEyknfy3eXTpgqAKZ9OGi+p7xRcjc2vzQAERsJLTh3qoNfqvPpwLls+SJJP21aWQCi5+z6ru6BZ2q9pa4AsOUqhaEtkzTUlMIARG1rbl7x6uneVFcASFLnyuImc7p4djUBiIFzplW2XKXp3lh3AEhSR0/pLySxchDQwky6ttBdqmuh3xkFgJlcdTTokey5xkoDECnnHu3YWrqs3rdP2w04mf6+zoMzLnxM0qJGtgcQiR1BWD2iY+XQ8/VuMKMzgDFd3QPPhM5+V9JII9sDaLqqzJ01k4NfajAAJKlzRfEh57Sq0e0BNI+ZXZDvLv/rTLdrOAAkqbCidIvkLp9NGwBmydzqXHfxhoY2bcb+i735L5p0TTPaAjATtjbfUzy30a1ndQYwptBTutYxRgCIlZO+nSsUz59NG00JAEkqrCh91UwXNqs9ADX15beWzrJlqs6mkaZcAoxXujl3rsxukJRtdtsAJJlbne8uX9GUpprRyESDfZ3HhS68U9JeUbQPpFRVzv1RfkW5acv1RRIAkjTU13Vg6Kr/4qSDo9oHkCLbZG55I119tTTtHsBE7d39zw7bvGPkVNeYZABTcO5RU3hEsw9+KcIzgDHOycp9uXMk+4akXNT7AxLEOemG/Gjp87ZKo1HsIPIAGDPQW1gayN0q6ai49gnMYc8707n1zuprVGSXABN19hQ357aWPuRMF0nRpBmQAKNO+mZuMHdI1Ae/FOMZwHjFvvyh5nSdpI/52D/QiszpwYqC87tWDGyJbZ9x7WgyxZvzJ5l0nUyH+awD8GyjzF2Z7y7fFfeOvQaAJLn1ypRLubPlbLWk/X3XA8TGuUclXZVfUb7XVwneA2CMW6+2UjF/pkkXSjrUdz1ARCqS7gudfb1zRfEh38W0TACMV7old5TC4A8ld5boOkQybHamW1XRLRMf0e1TSwbAmG23Le6aPzL8cZOWSfptSe2+awLqVJHcE1LwnVD6dmdPcbPvgibT0gEwnluvjlIp/xE5nWSmY+V0uKQ233UBuwxLetqkh525h3Ll/Pft/FeLvouazpwJgIncNzW/3Jk7zDlbatJBTnqXpLcF0lvc65OQTCxaitkbkVSU7DXJDci5omQDMvdzM9sSSlsyleqW9peGnrfVCn0XO1P/B10Z0J8mQB8OAAAAAElFTkSuQmCC"
 
 CONFIG_FILE = os.path.expanduser("~/.kastyn_gui.json")
 
@@ -54,8 +57,17 @@ class KastynGUI:
         self.process = None
         self.running = False
 
+        self._setup_icon()
         self._build_ui()
         self._check_agent()
+
+    def _setup_icon(self):
+        import base64, io
+        from PIL import Image, ImageTk
+        img_data = base64.b64decode(LOGO_B64)
+        img = Image.open(io.BytesIO(img_data)).resize((32, 32), Image.LANCZOS)
+        self._icon = ImageTk.PhotoImage(img)
+        self.root.iconphoto(True, self._icon)
 
     def _check_agent(self):
         # Check saved path first
@@ -142,6 +154,12 @@ class KastynGUI:
         # Header
         header = tk.Frame(self.root, bg=bg)
         header.pack(fill="x", padx=24, pady=(24, 0))
+        import base64, io
+        from PIL import Image, ImageTk
+        img_data = base64.b64decode(LOGO_B64)
+        img = Image.open(io.BytesIO(img_data)).resize((36, 36), Image.LANCZOS)
+        self._header_logo = ImageTk.PhotoImage(img)
+        tk.Label(header, image=self._header_logo, bg=bg).pack(side="left", padx=(0, 10))
         tk.Label(header, text="Kastyn", font=("Inter", 22, "bold"),
                  fg=amber, bg=bg).pack(side="left")
         tk.Label(header, text="  Agent Launcher", font=("Inter", 16),
